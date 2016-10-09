334cb72066f2155dafdbff23d5c3e19df00a8d8f
许宏辉 <18482292781@163.com>
2016-10-09 21:12:38 +0800

    Create BiTree.h

---
+++ BiTree.h
@@ -0,0 +1,16 @@
+typedef struct BiTreeNode
+{
+    int data;
+    struct BiTreeNode *lchild;
+    struct BiTreeNode *rchild;
+}BiTreeNode, *BiTree;
+
+void PreOrder(BiTree bt);
+void InOrder(BiTree bt);
+void PostOrder(BiTree bt);
+void LevelOrder(BiTree bt);
+void NRInOrder(BiTree bt);
+void PreInOrd(char preord[],char inord[],int i,int j,int k,int h,BiTree *t);
+void CreateBiTree(char preord[],char inord[],int n,BiTree root);
+int BitreeLeaf(BiTree bt);
+int BitreeDepth(BiTree bt);

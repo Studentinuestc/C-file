4143d5364af8da91d21148f43e023d1980854016
许宏辉 <18482292781@163.com>
2016-10-14 20:30:45 +0800

    Create HuffmanCoding.h

---
+++ HuffmanCoding.h
@@ -0,0 +1,13 @@
+#define MAXBIT 10
+#define MAXVALUE 1000
+typedef struct HNode
+{
+    int weight;
+    int parent,lchild,rchild;
+}HNode;
+
+typedef struct HNode
+{
+     int bit[MAXBIT];
+     int start;
+}HNode;
